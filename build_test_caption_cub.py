720244f4939fefb755a0c4ccf4421e86f63724c6
laizeqiang <laizeqiang@outlook.com>
2022-06-16 15:40:36 +0800

    update test code

--- build_test_caption_cub.py
+++ build_test_caption_cub.py
@@ -1,16 +1,27 @@
 import random
 from image_synthesis.data.cub200_dataset import Cub200Dataset
 
-dataset = Cub200Dataset('data/cub', phase='test')
-caption_dict = dataset.caption_dict
-captions = []
-for v in caption_dict.values():
-    captions += v
 
-print(len(captions))
+def oversample(dataset, num_samples):
+    dataset_len = len(dataset)
+    out = [dataset[random.randint(0, dataset_len-1)] for idx in range(num_samples)]
+    return out
+    
+    
 
-selected = random.sample(captions, 30000//8)
+def main():
+    dataset = Cub200Dataset('data/cub', phase='test')
+    caption_dict = dataset.caption_dict
+    captions = []
+    for v in caption_dict.values():
+        captions += v
 
-with open('cub_test_captions.txt', 'w') as f:
-    for caption in selected:
-        f.write(caption.strip() + '\n')
+    print(len(captions))
+        
+    selected = oversample(captions, 30000)
+
+    with open('cub_test_captions.txt', 'w') as f:
+        for caption in selected:
+            f.write(caption.strip() + '\n')
+            
+main()
